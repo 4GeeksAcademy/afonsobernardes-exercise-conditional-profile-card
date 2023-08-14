daf5f96b0fc59aa076ebb8683bb648968bacde70
Afonso Bernardes <84087794+AfonsoBernardes@users.noreply.github.com>
2023-08-14 18:54:24 +0000

    Postcard working.

--- src/js/app.js
+++ src/js/app.js
@@ -31,16 +31,53 @@ function render(variables = {}) {
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+          ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+
+          <h1>
+            ${variables.name ? variables.name : "First Name"} 
+            ${variables.lastname ? variables.lastname : "Last Name"}
+          </h1>
+    
+          <h2>${variables.role ? variables.role : "Role"}</h2>
+          
+          <h3>
+            ${variables.city ? variables.city : "City"},
+            ${variables.country ? variables.country : "Country"}
+          </h3>
+
+         <ul class="${variables.socialMediaPosition}">
+            <li>
+              <a target="_blank" href="${
+                variables.twitter
+                  ? variables.twitter
+                  : "https://twitter.com/4geeksacademy"
+              }"><i class="fab fa-twitter"></i></a>
+            </li>
+
+            <li>
+              <a target="_blank" href="${
+                variables.github
+                  ? variables.github
+                  : "https://github.com/4GeeksAcademy/"
+              }"><i class="fab fa-github"></i></a>
+            </li>
+
+            <li>
+              <a target="_blank" href="${
+                variables.linkedin
+                  ? variables.linkedin
+                  : "https://www.linkedin.com/in/afonso-bernardes/"
+              }"><i class="fab fa-linkedin"></i></a>
+            </li>
+
+            <li>
+              <a target="_blank" href="${
+                variables.instagram
+                  ? variables.instagram
+                  : "https://instagram.com/4geeksacademy"
+              }"><i class="fab fa-instagram"></i></a>
+            </li>
           </ul>
         </div>
     `;
